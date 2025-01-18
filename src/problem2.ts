e39c8c31c83a48a07c6a03e8dae1befe106bf266
Pranto Deb <rajprit0m@outlook.com>
2025-01-18 20:57:43 +0600

    Design a TypeScript function findLargestNumber that takes an array of numbers and returns the largest number in the array.

---
+++ src/problem2.ts
@@ -0,0 +1,13 @@
+{
+  /*
+    Design a TypeScript function findLargestNumber that takes an array of numbers and returns the largest number in the array.
+  */
+
+    const findLargestNumber = (arr: number[]): number => {
+        const largeNumber = Math.max(...arr)
+        return largeNumber
+    }
+    
+    console.log(findLargestNumber([10, 5, 8, 20, 3]))
+    
+}
